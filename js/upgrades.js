188675e98048d7f0929af8874b83438da3c260e5
AskPlays <adrian.skalevag@online.no>
2021-12-13 11:30:51 +0100

    fixed cost

--- js/upgrades.js
+++ js/upgrades.js
@@ -2,15 +2,22 @@ export default {
   "cog1": {
     "type": "basic",
     "currency": "number",
-    "ticks": 10,
-    "earn": (lvl) => lvl/10,
+    "earn": (lvl) => lvl,
+    "buy": (lvl) => 0,
     "cost": (lvl) => lvl*10,
   },
   "cog2": {
     "type": "tickSpeed",
     "currency": "number",
-    "ticks": Infinity,
-    "earn": (lvl) => 1,
-    "cost": (lvl) => lvl*10+100,
+    "earn": (lvl) => 0,
+    "buy": (lvl) => 10/(10+lvl),
+    "cost": (lvl) => lvl*50+100,
+  },
+  "cog3": {
+    "type": "gearRatio",
+    "currency": "number",
+    "earn": (lvl) => 0,
+    "buy": (lvl) => 5/(5+lvl),
+    "cost": (lvl) => lvl*50+10000,
   },
 }
--- main.js
+++ main.js
@@ -8,7 +8,8 @@ class Game {
   constructor() {
     this.number = 0;
     this.tick = 0;
-    this.tickSpeed = 10;
+    this.tickSpeed = 1;
+    this.gearRatio = 1;
     this.upgrades = {};
     this.states = {};
     this.buyMode = 0;
@@ -28,40 +29,50 @@ class Game {
     }
 
     this.load();
+    setInterval(()=>{this.save()}, 10000);
   }
   update() {
     this.tick++;
+    const prevNum = this.number;
     
     const keys = Object.keys(this.upgrades);
     for(const key of keys) {
       if(Upgrades[key].type == "basic") {
-        if(this.tick%Upgrades[key].ticks == 0) this.number += Upgrades[key].earn(this.upgrades[key]);
+        this.number += Upgrades[key].earn(this.upgrades[key])*this.gearRatio;
       }
     }
 
     // display
-    this.E.number.innerText = round(this.number);
-    this.E.tickSpeed.innerText = this.tickSpeed;
+    this.E.number.innerText = round(this.number) + ` (${round((this.number-prevNum)*this.tickSpeed)} n/s)`;
+    this.E.tickSpeed.innerText = round(this.tickSpeed);
     if(this.upgrades["cog1"]) {
-      this.E.cog.style.animationName = this.tickSpeed >= 30 ? "rotate": "rotateTick";
-      this.E.cog.style.animationTimingFunction = this.tickSpeed >= 30 ? "linear" : "ease-in-out";
-      this.E.cog.style.animationDuration = 10/this.tickSpeed+"s";
+      this.E.cog.style.animationName = this.tickSpeed >= 3 ? "rotate": "rotateTick";
+      this.E.cog.style.animationTimingFunction = this.tickSpeed >= 3 ? "linear" : "ease-in-out";
+      this.E.cog.style.animationDuration = 1/this.tickSpeed+"s";
     } else this.E.cog.style.animationDuration = "0s";
     // this.E.cog.style.transform = `rotate(${this.tick*20}deg)`;
-    if(this.tick/this.tickSpeed % 10 == 0) this.save();
   }
   upgrade(id, amnt=null) {
     const upgrade = Upgrades[id];
     const repeats = amnt!= null ? amnt : ([1, 10, 100, Infinity])[this.buyMode];
     let repeat = 0;
-    if(typeof this.upgrades[id] === "undefined") this.upgrades[id] = 0;
-    while(this[upgrade.currency] >= upgrade.cost(this.upgrades[id]) && repeat < repeats) {
+    // if(typeof this.upgrades[id] === "undefined") this.upgrades[id] = 0;
+    while(this[upgrade.currency] >= upgrade.cost(this.upgrades[id] || 0) && repeat < repeats) {
       this[upgrade.currency] -= upgrade.cost(this.upgrades[id]);
+      if(upgrade.type == "tickSpeed") this.changeSpeed(this.tickSpeed + upgrade.buy(this.upgrades[id]));
+      if(upgrade.type == "gearRatio") {
+        this.gearRatio = this.tickSpeed;
+        this.changeSpeed(1);
+        delete this.upgrades["cog1"];
+        delete this.upgrades["cog2"];
+        this.updateUpgrade("cog1");
+        this.updateUpgrade("cog2");
+      }
       this.upgrades[id]++;
-      if(upgrade.type == "tickSpeed") this.tickSpeed += upgrade.earn(this.upgrades[id]);
       repeat++;
     }
-    this.E[id].children[1].innerText = this.upgrades[id];
+    
+    this.updateUpgrade(id);
   }
   changeSpeed(spd) {
     this.tickSpeed = spd;
@@ -71,6 +82,10 @@ class Game {
   changeBuyMode() {
     this.buyMode = (this.buyMode+1)%BuyModes.length;
     this.E.buyMode.innerText = BuyModes[this.buyMode]; 
+  }
+  updateUpgrade(id) {
+    this.E[id].children[1].innerText = this.upgrades[id] || 0;
+    this.E[id].children[2].innerText = "Cost: "+Upgrades[id].cost(this.upgrades[id] || 0);
   }
   save() {
     localStorage.setItem('save', JSON.stringify({
@@ -85,9 +100,10 @@ class Game {
     const save = JSON.parse(localStorage.getItem('save'));
     if(save) {
       Object.assign(this, save);
-      const keys = Object.keys(this.upgrades);
+      const keys = Object.keys(Upgrades);
       for(const key of keys) {
         this.upgrade(key, 0);
+        this.updateUpgrade(key);
       }
       if(this.upgrades["cog1"]) this.E.enable.remove();
     }
